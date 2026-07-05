62df0c236cc03ff2698f42bd98bea1b3d5ca493d
= <sultaninstem004@gmail.com>
2026-07-05 16:48:44 -0500

    readme

--- conductor.py
+++ conductor.py
@@ -9,12 +9,19 @@ class Conductor:
         self.lattice_spacing = lattice_spacing
         self.n_side = size // lattice_spacing
         self.q_proton = +1
+        self.proton_radius = 2
         self.q_electron = -1
 
         ### Initializing an array of protons
-        x, y = np.meshgrid(np.arange(self.n_side) * lattice_spacing, np.arange(self.n_side) * lattice_spacing)
+        x, y = np.meshgrid(np.arange(self.n_side) * lattice_spacing + pos[0] + self.proton_radius, np.arange(self.n_side) * lattice_spacing + pos[1] + self.proton_radius)
         self.protons = np.column_stack([x.ravel(), y.ravel()]) 
-        ### Initital positions of electrons are near their respective protons with a small offset 
-        self.electrons = []
+        ### Initital positions of electrons are near their respective protons with a small random offset 
+        self.electrons = self.protons + np.random.uniform(-1, 1, self.protons.shape)
+
     def show(self, screen): 
+        for i in range(0, len(self.protons)): 
+            x,y = self.protons[i]
+            pygame.draw.circle(screen, (255, 50, 50), (x,y), self.proton_radius)
+            x,y = self.electrons[i]
+            pygame.draw.circle(screen, (50, 50, 255), (x,y), 1)
         pygame.draw.rect(screen, self.color, (self.pos[0], self.pos[1], self.size, self.size), 1)
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 ### UNITS 
 
+Overdamped massless relaxation (gradient descent)
+
+$$
+E = mc^2
+r^(t+1) = r^(t) + eta *F^(t)
+$$
